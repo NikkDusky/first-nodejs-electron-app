b7befdff04176c70f6723d6a4e2320aea5bc7e4a
NikkDusky <nikkdusky@gmail.com>
2021-12-27 09:14:04 +0500

    some little changes

--- src/js/appFunctions.js
+++ src/js/appFunctions.js
@@ -1,6 +1,7 @@
 const { ipcRenderer } = require('electron')
 const ipc = ipcRenderer
 const mySidebar = document.getElementById('mySidebar')
+const linksBar = document.getElementById('linksBar')
 var isLeftMenuActive = true
 
 minimizeBtn.addEventListener('click', () => {
@@ -17,9 +18,13 @@ closeBtn.addEventListener('click', () => {
 
 showHideMenus.addEventListener('click', () => {
     if(isLeftMenuActive){
+        setTimeout(function(){linksBar.style.display = 'none'}, 15);
+        mySidebar.style.opacity = '0%'
         mySidebar.style.width = '0px'
         isLeftMenuActive = false
     } else {
+        setTimeout(function(){linksBar.style.display = 'inline-block'}, 150);
+        mySidebar.style.opacity = '100%'
         mySidebar.style.width = '280px'
         isLeftMenuActive = true
     }
